6c8017957466be3ce2566b51e5bb430f4d1e0cb8
H10yiling <lucky0919503014@gmail.com>
2022-09-22 18:39:11 +0800

    0922 push

--- LocalizationPhotoDemo/LocalizationDemo/MainViewController/MainViewController.swift
+++ LocalizationPhotoDemo/LocalizationDemo/MainViewController/MainViewController.swift
@@ -9,21 +9,10 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    @IBOutlet weak var hiLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        hiLabel.text = self.transalte(LocalizedString: "Hi")
-        
-    }
-    
-    /// 簡便翻譯
-    /// - Parameters:
-    ///   - key: 在 LocalizableStrings 裡面定義的 Key
-    /// - Returns: 在 LocalizableStrings 裡面定義的 Value
-    func transalte(LocalizedString key: String) -> String {
-        // 設定多國語系，填入key值
-        return NSLocalizedString(key, comment: "")
+        iconImageView.image = UIImage(named: NSLocalizedString("language", comment: ""))
     }
 }
