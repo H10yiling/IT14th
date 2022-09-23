f6f48e5a011a6e3fb3caa877d5d96012e5c7a534
H10yiling <lucky0919503014@gmail.com>
2022-09-24 02:07:10 +0800

    0923-2 push

--- LocalizationAppInsideChangeDemo/LocalizationDemo/Localize.swift
+++ LocalizationAppInsideChangeDemo/LocalizationDemo/Localize.swift
@@ -11,13 +11,14 @@ class Localize: NSObject {
     
     static let shared = Localize()
     
-    /// 取得 Label 值
+    /// 依語系檔裡的 key 取得 value 值（用於 Label 的 Text）
     ///
     /// - Parameters:
     ///   - withText: 多國語系檔的 key 值
     ///   - withLocalizationFileNmae: 多國語系檔的檔名
     /// - Returns: String
     func localizedText(withText key: String, withLocalizationFileNmae localizationFileNmae: String) -> String {
+        
         // 取得 Bundle 下的的多國語系檔
         let path = Bundle.main.path(forResource: localizationFileNmae, ofType: "lproj")
         let bundle = Bundle(path: path!)
@@ -26,18 +27,19 @@ class Localize: NSObject {
         return NSLocalizedString(key, tableName: nil, bundle: bundle!, value: "", comment: "")
     }
     
-    /// 依語系檔裡的 key 取得 value 值
+    /// 依語系檔裡的 key 取得 value 值（用於 UIImageView）
     ///
     /// - Parameters:
     ///   - withImageName: 多國語系檔的 key 值
     ///   - withLocalizationFileNmae: 多國語系檔的檔名
-    /// - Returns: String
+    /// - Returns: UIImage
     func localizedImage(withImageName key: String, withLocalizationFileNmae localizationFileNmae: String) -> UIImage {
+        
         // 取得 Bundle 下的的多國語系檔
         let path = Bundle.main.path(forResource: localizationFileNmae, ofType: "lproj")
         let bundle = Bundle(path: path!)
 
         // 依 key 值和 Bundle 的多國語系檔取得對應的 value
-        return UIImage(named: NSLocalizedString(key,bundle: bundle!, comment: "")) ?? UIImage(systemName: "questionmark.folder")!
+        return UIImage(named: NSLocalizedString(key,bundle: bundle!, comment: ""))!
     }
 }
